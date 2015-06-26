c7cce76a89490828462c76750359bc81c6b52369
Anders Pearson <anders@columbia.edu>
2015-06-26 15:36:18 +0200

    added smoketest to check for required settings

---
+++ contactus/smoke.py
@@ -0,0 +1,12 @@
+from django.conf import settings
+
+try:
+    from smoketest import SmokeTest
+except ImportError:
+    # django-smoketest isn't installed
+    SmokeTest = object
+
+
+class ExpectedSettings(SmokeTest):
+    def test_contact_us_email(self):
+        self.assertIsNotNone(settings.CONTACT_US_EMAIL)
