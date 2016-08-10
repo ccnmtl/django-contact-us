b949407bd3c556d26de80a2e8444de5d50b0e7fa
Anders Pearson <anders@columbia.edu>
2016-08-10 15:49:01 +0100

    :bookmark: release 0.3.0

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="django-contact-us",
-    version="0.2.0",
+    version="0.3.0",
     author="Susan Dreher",
     author_email="sdreher@columbia.edu",
     url="https://github.com/ccnmtl/django-contact-us",
